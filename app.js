a1a8f26e2245a252ac369a5d8ef56148b6788de7
tinotendnyashanu <tinotendanyash@gmail.com>
2023-10-14 02:27:49 +0200

    ready for deplyment

--- app.js
+++ app.js
@@ -1,36 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const bodyParser = require("body-parser");
-require("dotenv").config();
 
 const uri = process.env.ATLAS_URI;
 const app = express();
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error:")
+);
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+});
 
 app.use(cors());
 app.use(bodyParser.json());
